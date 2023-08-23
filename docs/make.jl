ce680c88fd05a877b74467f7e5652ce8f133a13f
Chibuikem709 <16184620+Chibuikem709@users.noreply.github.com>
2023-08-23 17:37:07 -0400

    Fixed repo name error

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,11 @@ push!(LOAD_PATH,"../src/")
 using nclusion
 using Documenter
 makedocs(
-         sitename = "nclusion.jl",
+         sitename = "nclusion",
          modules  = [nclusion],
          pages=[
                 "Home" => "index.md"
                ])
 deploydocs(;
-    repo="github.com/microsoft/nclusion.jl",
+    repo="github.com/microsoft/nclusion",
 )
